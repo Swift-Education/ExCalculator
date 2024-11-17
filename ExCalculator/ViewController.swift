d4479153769362fc90b5a5a643c51450809c8dcb
kangddong <rkdehddud96@naver.com>
2024-11-17 23:02:33 +0900

    [Lv1] result Label 추가

--- ExCalculator/ViewController.swift
+++ ExCalculator/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let resultLabel: UILabel = {
+        let label: UILabel = .init()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "12345"
+        label.textAlignment = .right
+        label.font = UIFont.boldSystemFont(ofSize: 60)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(resultLabel)
+        NSLayoutConstraint.activate([
+            resultLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            resultLabel.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
 
