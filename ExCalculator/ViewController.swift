3a3a8bfed51087c64af6485b436f04b56094e2d3
kangddong <rkdehddud96@naver.com>
2024-11-18 12:41:21 +0900

    [Lv4] Set을 활용한 연산 버튼 오렌지 색상 지정

--- ExCalculator/ViewController.swift
+++ ExCalculator/ViewController.swift
@@ -28,12 +28,14 @@ class ViewController: UIViewController {
             resultLabel.heightAnchor.constraint(equalToConstant: 100)
         ])
         
-        let stackView1 = makeHorizontalStackView(makeKeypadButton(["7", "8", "9", "+"]))
-        let stackView2 = makeHorizontalStackView(makeKeypadButton(["4", "5", "7", "-"]))
-        let stackView3 = makeHorizontalStackView(makeKeypadButton(["1", "2", "3", "*"]))
-        let stackView4 = makeHorizontalStackView(makeKeypadButton(["AC", "0", "=", "/"]))
-        
-        let stackView = makeVerticalStackView([stackView1, stackView2, stackView3, stackView4])
+        let buttonTitles: [[String]] = [
+            ["7", "8", "9", "+"],
+            ["4", "5", "7", "-"],
+            ["1", "2", "3", "*"],
+            ["AC", "0", "=", "/"],
+        ]
+        let hStackViews = buttonTitles.map { makeHorizontalStackView(makeKeypadButton($0))}
+        let stackView = makeVerticalStackView(hStackViews)
         
         view.addSubview(stackView)
         NSLayoutConstraint.activate([
@@ -48,10 +50,15 @@ class ViewController: UIViewController {
             let button: UIButton = UIButton()
             button.setTitle($0, for: .normal)
             button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            button.frame.size = CGSize(width: 80, height: 80)
-            button.layer.cornerRadius = 40
-            button.heightAnchor.constraint(equalToConstant: 80).isActive = true
+            
+            let grayColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            let orangeColor = UIColor.orange
+            let orangeTitles: Set<String> = ["+", "-", "*", "/", "=", "AC"]
+            button.backgroundColor = orangeTitles.contains($0) ? orangeColor : grayColor
+            
+            let height: CGFloat = 80
+            
+            button.heightAnchor.constraint(equalToConstant: height).isActive = true
             button.widthAnchor.constraint(equalTo: button.heightAnchor).isActive = true
             return button
         }
