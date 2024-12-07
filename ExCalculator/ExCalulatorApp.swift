6ff657279198c5f2e189f008ef68b0665dbf3f66
kangddong <rkdehddud96@naver.com>
2024-12-07 12:45:34 +0900

    [ADD] SwftUI entry point ExCalculatorApp

---
+++ ExCalculator/ExCalulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExCalulatorApp.swift
+//  ExCalculator
+//
+//  Created by 강동영 on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ExCalulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
