c8c8d37f56dabc5a9fd6a62a8c3eb91519ce1bbe
kangddong <rkdehddud96@naver.com>
2024-12-07 14:43:38 +0900

    [ADD] NewCalculator View 생성

---
+++ ExCalculator/View/NewCalulatorView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCalulatorView.swift
+//  ExCalculator
+//
+//  Created by 강동영 on 12/7/24.
+//
+
+import SwiftUI
+
+struct NewCalulatorView: View {
+    var body: some View {
+        Text("0")
+    }
+}
+
+#Preview {
+    NewCalulatorView()
+}
