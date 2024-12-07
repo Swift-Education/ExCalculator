59d3b03008ade328255e557e07d1f84595d9f244
kangddong <rkdehddud96@naver.com>
2024-12-07 19:15:42 +0900

    [ADD] NewKeypadView

---
+++ ExCalculator/View/NewKeypadView.swift
@@ -0,0 +1,18 @@
+//
+//  NewKeypadView.swift
+//  ExCalculator
+//
+//  Created by 강동영 on 12/7/24.
+//
+
+import SwiftUI
+
+struct NewKeypadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewKeypadView()
+}
